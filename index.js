31b412e8a87d993a4f674d74b2c32ccdefc1f150
Adam Novak <interfect@gmail.com>
2017-11-07 20:46:47 -0800

    Cool blue Substack grid

--- index.js
+++ index.js
@@ -40,46 +40,86 @@ const fragSolid = glsl`
   }
 `
 
+// This shader makes things wavy in Y
+const vertWavy = glsl`
+  precision mediump float;
+  
+  // Camera stuff
+  uniform mat4 proj;
+  uniform mat4 model;
+  uniform mat4 view;
+  
+  attribute vec3 position;
+  
+  void main () {
+    // Apply a sin wave to the mesh in y
+    vec3 modPos = vec3(position.x, position.y + sin(position.x), position.z);
+    // Then apply the transform
+    gl_Position = proj * view * model * vec4(modPos, 1);
+  }
+`
+
+// This shader does substack's screen-projected-lines wireframe
+const vertWireframe = glsl`
+  precision mediump float;
+  
+  #pragma glslify: linevoffset = require('screen-projected-lines')
+  
+  // Camera stuff
+  uniform mat4 proj;
+  uniform mat4 model;
+  uniform mat4 view;
+  
+  // We need the screen aspect ratio
+  uniform float aspect;
+  
+  attribute vec3 position;
+  
+  // We also need the position of the "next" vertex to draw a line to
+  attribute vec3 nextpos;
+  
+  // And a float describing the direction to it (?)
+  attribute float direction;
+  
+  void main () {
+    mat4 proj_combined = proj * view;
+    vec4 p = proj_combined*vec4(position, 1);
+    vec4 n = proj_combined*vec4(nextpos, 1);
+    vec4 offset = linevoffset(p, n, direction, aspect);
+    // Just do normal wireframe
+    gl_Position = p + offset*0.02;
+    
+  }
+`
+
 
 function createOcean() {
   // Define a grid
   const size = 10;
-  const mesh = grid(size, size)
+  var mesh = grid(size, size)
 
-  // Convert mesh from 2d to 3d  
-  const positions = mesh.positions.map((vec2) => {
-    vec2.push(0)
-    return vec2
+  // Convert mesh from 2d to 3d
+  mesh.positions = mesh.positions.map((vec2) => {
+    // Put the mesh in the XZ plane; Y is up.
+    return [vec2[0], 0, vec2[1]]
   })
-  const cells = mesh.cells
+  
+  // Wireframe it for screen-projected-lines
+  mesh = wireframe(mesh)
   
   // Define the draw to do and return it
   return regl({
     frag: fragSolid,
-    vert: glsl`
-      precision mediump float;
-      
-      // Camera stuff
-      uniform mat4 proj;
-      uniform mat4 model;
-      uniform mat4 view;
-      
-      attribute vec3 position;
-      
-      void main () {
-        // Apply a sin wave to the mesh
-        vec3 modPos = vec3(position.x, position.y, position.z + sin(position.x));
-        // Then apply the transform
-        gl_Position = proj * view * model * vec4(modPos, 1);
-      }
-    `,
+    vert: vertWireframe,
+    // Copy all the wireframe stuff over
     attributes: {
-      // Mesh positions are in 2d so we put them at a Z
-      position: positions
+      position: mesh.positions,
+      nextpos: mesh.nextPositions,
+      direction: mesh.directions
     },
-    elements: cells,
+    elements: mesh.cells,
     uniforms: {
-      color: [1, 0, 0, 1],
+      color: [0, 0.7, 0.8, 1],
       // Use a matrix stack ripped from the documentation.
       // This is how we stick the image into our window as a function of window size.
       proj: ({viewportWidth, viewportHeight}) =>
@@ -90,18 +130,23 @@ function createOcean() {
           1000),
       // The model is at the origin
       model: mat4.identity([]),
-      // This is the camera matrix. It's the spinny one from the documentation.
+      // This is the camera matrix. It's the spinny one from the documentation, modified to spin gooder
       view: ({tick}) => {
         const t = 0.01 * tick
-        const radius = 20;
-        const height = 10
+        const radius = 10
+        const height = 2.5
+        const center = [5, 0, 5]
         return mat4.lookAt([],
           // Here is our eye
-          [radius * Math.cos(t), height, radius * Math.sin(t)],
+          [center[0] + radius * Math.cos(t), center[1] + height, center[2] + radius * Math.sin(t)],
           // Here is where we look
-          [0, 5, 0],
+          center,
           // This is up
           [0, 1, 0])
+      },
+      // We alsop need the aspect ratio for screen-projected-lines
+      aspect: ({viewportWidth, viewportHeight}) => {
+        return viewportWidth / viewportHeight
       }
     }
   })
